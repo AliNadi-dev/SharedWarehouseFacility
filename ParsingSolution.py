d16f436f1567540a61b9b6ccb17e1d1a4fdf7e13
Ali Nadi <a88nadi@gmail.com>
2021-05-10 13:13:49 +0200

    parsing solution in progress

--- ParsingSolution.py
+++ ParsingSolution.py
@@ -10,7 +10,7 @@ Created on Wed May  5 01:10:02 2021
 """
 
 import numpy as np 
-
+import pandas as pd
 
 def Parse(model,randsol):
 
@@ -18,11 +18,11 @@ def Parse(model,randsol):
     w=model['aisle_width']
     v=model['cross_aisle_width']
     S=model['S']
+    g=model['g']
+    h=model['h']
+
     l=randsol['NumberOfLevels']
-
-
     q=randsol['SequenceOfCompanies']
-
     n=randsol['NumberOfAisles']
     
     
@@ -34,7 +34,7 @@ def Parse(model,randsol):
 
     To=np.concatenate((delimiterposition,[Z+l-1]),axis=0)
     
-
+# %% decompose assignment into the level and calculate cordinates of the areas 
     L={}
     X0={}
     Xend={}
@@ -51,22 +51,22 @@ def Parse(model,randsol):
         if L[str(j)].size >0:
             for k in range(len(L[str(j)])-1):
                 X0[str(j)][k+1]=X0[str(j)][k]+n[L[str(j)][k]-1]*w
-                Xend[str(j)][k]=X0[str(j)][k+1]
+                Xend[str(j)][k]=X0[str(j)][k]+n[L[str(j)][k]-1]*w
                 if j==0:
                     
-                    Yend[str(j)][k]=Y0[str(j)][k]+S/(n[L[str(j)][k]-1]*v)
+                    Yend[str(j)][k]=Y0[str(j)][k]+(S/(n[L[str(j)][k]-1])+(2*v))
                     if k ==len(L[str(j)])-2:
                         Xend[str(j)][k+1]=X0[str(j)][k+1]+n[L[str(j)][k+1]-1]*w                     
-                        Yend[str(j)][k+1]=Y0[str(j)][k+1]+S/(n[L[str(j)][k+1]-1]*v)
+                        Yend[str(j)][k+1]=Y0[str(j)][k+1]+(S/(n[L[str(j)][k+1]-1])+(2*v))
                 else:
                     Y0[str(j)][k]=min(Yend[str(j-1)])
-                    Yend[str(j)][k]=Y0[str(j)][k]+S/(n[L[str(j)][k]-1]*v)  
+                    Yend[str(j)][k]=Y0[str(j)][k]+(S/(n[L[str(j)][k]-1])+(2*v))  
                     if k ==len(L[str(j)])-2:
                         Xend[str(j)][k+1]=X0[str(j)][k+1]+n[L[str(j)][k+1]-1]*w
                         Y0[str(j)][k+1]=min(Yend[str(j-1)])
-                        Yend[str(j)][k+1]=Y0[str(j)][k+1]+S/(n[L[str(j)][k+1]-1]*v)
+                        Yend[str(j)][k+1]=Y0[str(j)][k+1]+(S/(n[L[str(j)][k+1]-1])+(2*v))
 
-            
+ # %%           
                 
 
                         
