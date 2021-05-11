ebb0f679d454888c83e1b777e7f14341bd307a71
Ali Nadi <a88nadi@gmail.com>
2021-05-11 19:39:26 +0200

    .

--- ParsingSolution.py
+++ ParsingSolution.py
@@ -86,18 +86,3 @@ def Parse(model,randsol):
                         
 
     return sol
-# %%
-# in this script we integrate all the modules to create, solve and visulize the SWF problem
-
-#%% import modules 
-import LoadModel
-import RandomSolution
-#import ParsingSolution
-#%% Load model 
-model=LoadModel.SelectModel()
-
-#%% create random solution 
-randsol=RandomSolution.Create(model)
-
-# %% parse solution 
-sol=Parse(model,randsol)
