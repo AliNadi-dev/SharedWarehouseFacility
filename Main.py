2f27ad2761b2af77f220aaf20f777bd5b1334da7
Ali Nadi <a88nadi@gmail.com>
2021-05-11 17:43:35 +0200

    main script added

---
+++ Main.py
@@ -0,0 +1,11 @@
+# in this script we integrate all the modules to create, solve and visulize the SWF problem
+
+#%% import modules 
+import LoadModel
+import RandomSolution
+import ParsingSolution
+#%% Load model 
+model=LoadModel.SelectModel()
+
+#%% create random solution 
+randsol=RandomSolution.Create(model)
